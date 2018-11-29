8c053ce82f0a96de5f9e755fb4fbbcc0a5f695f0
ceccs17d11 <43630274+ceccs17d11@users.noreply.github.com>
2018-11-29 15:16:09 +0530

    Update 10_Binarysearch.c

--- 10_Binarysearch.c
+++ 10_Binarysearch.c
@@ -1,43 +1,85 @@
 /*Archana Rose Biju
   S3 D 04
   Binary Search*/
+#include <stdio.h>
 
-#include<stdio.h>
-void main()
-{ int f,l,n,mid,a[10],i,s,temp,j;
-  printf("Enter no of elements:");
-  scanf("%d",&n);
-  printf("Enter elements:");
-  for(i=0;i<n;i++)
-	scanf("%d",&a[i]);
-  printf("Enter element to search:");
-  scanf("%d",&s);
-  for(i=0;i<n;i++)	
-	{ for(j=0;j<n;j++)
-		{ if(a[i]<a[j])
-			{ temp=a[i];
-			  a[i]=a[j];
-			  a[j]=temp;
+int selectionSort(int array[], int size) {
+	int i, j, pos, temp;
+	for (i=0; i<size; i++) {
+		pos = i;
+
+		for (j=i; j<size; j++) {
+			if (array[j] < array[pos]) {
+				pos =j;
 			}
 		}
+		if (i != pos) {
+			temp = array[i];
+			array[i] = array[pos];
+			array[pos] = temp;
+		}
 	}
-  for(i=0;i<n;i++)
-	printf("%d \t",a[i]);
-  f=0;
-  l=n-1;
-  mid=(f+l)/2;
-  while(f<=l)
-	{ if(a[mid]<s)
-		f=mid+1;
-          else if(a[mid]==s)
-		{  printf("Element found:%d",mid+1);
-              	   break;
-	        }
-          else 
-		l=mid-1;
-          mid=(f+l)/2;
+}
+
+int main() {
+	int i, beg, mid, last, found=0, array[100], element, size;
+
+	printf("Enter number of elements : ");
+	scanf("%d", &size);
+
+	printf("Enter elements\n");
+	for (i=0; i<size; i++) {
+		scanf("%d", &array[i]);
 	}
 
-  if (f>l)
-      printf("Not found!");
+	selectionSort(array, size);
+
+	printf("Sorted array\n");
+	for (i=0; i<size; i++) {
+		printf("%d\n", array[i]);
+	}
+	printf("Enter element to search : ");
+	scanf("%d", &element);
+
+	beg = 0;
+	last = size-1;
+	while (last >= beg) {
+		mid = (beg + last)/2;
+		if (array[mid] == element) {
+			found = 1;
+			break;
+		} else if (array[mid] > element) {
+			last = mid - 1;
+		} else {
+			beg = mid + 1;
+		}
+	}
+
+	if (found) {
+		printf("Element found at position %d\n", mid+1);
+	} else {
+		printf("Element not found\n");
+	}
 }
+
+/*  output
+Enter number of elements : 7
+Enter elements
+4
+6
+8
+2
+3
+7
+1
+Sorted array
+1
+2
+3
+4
+6
+7
+8
+Enter element to search : 6
+Element found at position 5
+*/
